1703cd4f879dcc0cf4b85ca848f36af913ea01cb
Dylan Westover <dylan.j.westover@vanderbilt.edu>
2014-05-23 11:26:45 -0700

    Update controls.js

--- controls.js
+++ controls.js
@@ -20,3 +20,15 @@
             this._div = L.DomUtil.create('div', 'zoomOut');
             return this._div;
         }
+        
+//possible fix to the zoomOut method
+//_onDoubleClick: function (e) {
+ // 		var map = this._map,
+ //This next line was a previous implementation which was taken out.
+ //-		    zoom = map.getZoom() + 1;
+ //this next line was added as a revision
+ //+		    zoom = map.getZoom() + (e.originalEvent.shiftKey ? -1 : 1);
+  
+  	//	if (map.options.doubleClickZoom === 'center') {
+  		//	map.setZoom(zoom);
+//Of course, this must be changed to run with our map implementation, but I think this is a good starting point for a fix.
