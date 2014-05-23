e3a8efef41a543c9a0166e9feb6f5f1516da496d
Ethan Raymond <ethan.raymond14@gmail.com>
2014-05-23 13:11:10 -0400

    fifth commit

---
+++ controls.js
@@ -0,0 +1,22 @@
+		var info = L.control();
+
+		info.onAdd = function (map) {
+		    this._div = L.DomUtil.create('div', 'info'); // create a div with a class "info"
+		    this.update();
+		    return this._div;
+		};
+
+		info.update = function (props) {
+    		this._div.innerHTML = (props ?
+        	'<h4>' + props.SOVEREIGNT + '</h4>'
+        	: 'Hover over a country');
+		};
+
+       var zoomOut = L.control({
+        //    'onclick': zoomedOut()
+        });
+
+        zoomOut.onAdd = function (map) {
+            this._div = L.DomUtil.create('div', 'zoomOut');
+            return this._div;
+        }
---
+++ functions.js
@@ -0,0 +1,99 @@
+        function removeAid() {
+            if (document.switches.aid.checked) {
+                map.removeLayer(aid);
+                aidLabel.style.backgroundColor = "#303030";
+            }
+            else {
+                map.addLayer(aid);
+                aidLabel.style.backgroundColor = '#FF3030';
+            }
+        }
+
+        function removeEcon()
+        {
+            if (document.switches.economic.checked) {
+                map.removeLayer(economic);
+                econ.style.backgroundColor = '#303030';
+            }
+            else {
+                map.addLayer(economic);
+                econ.style.backgroundColor = '#3366FF';
+            }
+        }
+
+        function removeEnviro() {
+            if (document.switches.environment.checked) {
+                map.removeLayer(environment);
+                enviro.style.backgroundColor = '#303030';
+            }
+            else {
+                map.addLayer(environment);
+                enviro.style.backgroundColor = '#00CC00';
+            }
+        }
+
+        function removeHR() {
+            if (document.switches.hr.checked) {
+                map.removeLayer(hr);
+                human_rights.style.backgroundColor = '#303030';
+            }
+            else {
+                map.addLayer(hr);
+                human_rights.style.backgroundColor = '#FFCC00';      
+            }
+        }
+
+        function removePolit() {
+            if (document.switches.political.checked) {
+                map.removeLayer(political);
+                polit.style.backgroundColor = '#303030';
+            }
+            else {
+                map.addLayer(political);
+                polit.style.backgroundColor = '#990099';
+            }
+        }
+
+        function highlightFeature(e) {
+            var layer = e.target;
+
+            layer.setStyle({
+                weight: 3,
+                color: '#666',
+                dashArray: '',
+                fillColor: 'white',
+                fillOpacity: 0.7,
+            });
+            if (!L.Browser.ie && !L.Browser.opera) {
+                layer.bringToFront();
+            }
+            info.update(layer.feature.properties);
+        }
+
+        function resetHighlight(e) {
+            geojson.resetStyle(e.target);
+            info.update();
+        }
+
+        function zoomToFeature(e) {
+            map.fitBounds(e.target.getBounds());
+        }
+
+        function onEachFeature(feature, layer) {
+            layer.on({
+                mouseover: highlightFeature,
+                mouseout: resetHighlight,
+                click: zoomToFeature
+            });
+        }
+
+        function addHover(marker) {
+            marker.on('mouseover', function(){marker.openPopup();});
+            marker.on('mouseout', function(){marker.closePopup();});
+        }
+
+        function attachHover(markers) {
+            for (var i = 0; i < markers.length; i++) {
+                addHover(markers[i]);
+            }
+        }
